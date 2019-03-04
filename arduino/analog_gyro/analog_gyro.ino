4ab3970492b5be289c7885b23389ba267924f718
Vadim <akimovvadim779@gmail.com>
2019-03-04 18:45:09 +0300

    Added class LISY300AL for analog gyro

--- arduino/analog_gyro/analog_gyro.ino
+++ arduino/analog_gyro/analog_gyro.ino
@@ -1,9 +1,6 @@
-#define GYRO_SENSITIVITY 0.0033;
+#define GYRO_SENSITIVITY 0.0033; //0.0033
 
-#define ANALOG_SENSITIVITY 0.0047
-// Arduino USB'Ye baglandıgında 4.575 V ile
-//çalışıyor. (Aref=4575) Analog çözünürlük
-//10bit dolayısıyla hassaslık=4575/1024
+#define ANALOG_SENSITIVITY 0.0047 //0.0047
 
 int sensor = A0; // analog gyro port
 int change = 0; // ATD conversion change
@@ -16,48 +13,29 @@ void setup(){
 }
 
 void loop(){
-
-  //Döngüde o an oluşan analog değeri gyro_x değişkenine atıyoruz.
   int gyro_x = analogRead(sensor);
+//  Serial.println(gyro_x);
+  if(abs(gyro_x-337)>3)
+    change = gyro_x-337; 
 
-//Gyro Hareket etmez ikenki değeri ile
-// hareket sonrası değişim ditital olarak
-// 3 birimin üstünde değilse bunu yok sayıyoruz.
-//Değişim dijital olarak 3 birimin üstünde ise
-//change değişkenine değişimi atıyoruz.
-//Bendeki gyro şu anda hareket etmez iken
-//dijital olarak 352 değerini gösteriyor.
-
-  if(abs(gyro_x-358)>3)
-    change = gyro_x-358; 
-
-  //Son döngüde harcanan zamanı hesaplamak
-  //için lasttok ve timetook degerlerini güncelliyoruz.
   lasttook = timetook;
   timetook = millis();
 
-    float time = (float(timetook)-float(lasttook))/1000.0;                             
-    //Son döngüde geçen zamanı hesaplıyoruz (saniye cinsinden)
+  float time = (float(timetook)-float(lasttook))/1000.0;                             
 
-    float degchange = change * time *  ANALOG_SENSITIVITY / GYRO_SENSITIVITY ;
-    //Derece olarak değişim miktarını hesaplıyoruz. 
+  float degchange = change * time *  ANALOG_SENSITIVITY / GYRO_SENSITIVITY ;
 
-    position = position+degchange;
-  //position değerinin değerini derece cinsinden
-  //buldugumuz değer kadar arttırıyoruz.
+  position = position+degchange;
 
-    position = makecardinal(position);
-  //360 dereceden büyük 0 dereceden küçük olma
-  // durumları için dönüşümleri yapıyoruz.
+  position = makecardinal(position);
 
-  Serial.println(position);  //Derece bilgisini seri porta yazdırıyoruz.
+  Serial.println(position);
 
   change=0;
 
   delay(10);
 }
 
-//Derece Dönüşüm fonksiyonu
 float makecardinal(float position){
   if(position > 360.00){
       position = position-360.00;
--- arduino/scetch-3/scetch-3.ino
+++ arduino/scetch-3/scetch-3.ino
@@ -1,16 +1,17 @@
 #include "STL3GD20.h"
 
-L3GD20 gyro(0x6B);
+//L3GD20 gyro(0x6B);
 
 void setup() {
   Serial.begin(9600);
-  gyro.setupL3GD20(2000);
+//  gyro.setupL3GD20(2000);
+  
 }
 
 void loop() {
-  gyro.updateData();
-  Serial.print(gyro.getX()); Serial.print(' ');
-  Serial.print(gyro.getY()); Serial.print(' ');
-  Serial.print(gyro.getZ()); Serial.println(' ');
-  delay(100);
+//  gyro.updateData();
+//  Serial.print(gyro.getX()); Serial.print(' ');
+//  Serial.print(gyro.getY()); Serial.print(' ');
+//  Serial.print(gyro.getZ()); Serial.println(' ');
+//  delay(100);
 }
